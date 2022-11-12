2a68266a37e869bf1a7538a2d70a19bd230c72f8
fancn21th <fancn21th@gmail.com>
2022-11-12 15:30:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const path = require("path");
+const copy = require("copy-template-dir");
+
+const vars = {
+  name: "foo",
+};
+
+const inDir = path.join(__dirname, `template`);
+const outDir = path.join(process.cwd(), `output`);
+
+console.log({ inDir });
+
+copy(inDir, outDir, vars, (err, createdFiles) => {
+  if (err) throw err;
+  createdFiles.forEach((filePath) => console.log(`Created ${filePath}`));
+  console.log("done!");
+});
